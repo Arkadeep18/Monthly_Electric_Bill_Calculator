e8ef30d7dae30118a129fec1d4811a5dc03a1f33
Arka <146548597+Arkadeep18@users.noreply.github.com>
2024-01-30 00:19:57 +0530

    Update question_6.java

--- question_6.java
+++ question_6.java
@@ -8,16 +8,16 @@ public class question_6 {
 		double totalAmount;
 		double discount=0;
 		
-		System.out.println("Do you want to pay online (y/n)?");
+		System.out.println("Do you want to pay online (y/n)?");	   //if "n" then the customer will redirected to the totalamount section(without discount).
 		String payonline = sc.next();
 		
-		if(unit<=50) {
+		if(unit<=50) {			
 			totalAmount = unit*3;
 		}
 		else if(unit<=200) {
 			totalAmount = 50*3+(unit-50)*4.80;
 		}
-		else if(unit<=400) {
+		else if(unit<=400) {					
 			totalAmount = 50*3+150*4.80+(unit-200)*5.80;
 		}
 		else {
@@ -25,12 +25,12 @@ public class question_6 {
 		}
 		System.out.println("₹"+totalAmount); 
 		
-		if(payonline.equalsIgnoreCase("y")) {
+		if(payonline.equalsIgnoreCase("y")) { 		//Discount section
 			discount = totalAmount*0.03;
 			totalAmount = totalAmount-discount;
 		}
-		System.out.println("Discount: ₹"+discount);
-		System.out.println("Amount payable: ₹"+totalAmount);
+		System.out.println("Discount: ₹"+discount);		//Discount
+		System.out.println("Amount payable: ₹"+totalAmount);	//Amount payable after discount
 	}
 
 }
